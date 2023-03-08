3eb8a48d496f06374bd1923d8ec7134a10e8b472
Abstractn <58334120+Abstractn@users.noreply.github.com>
2023-03-08 16:57:28 +0100

    added basic data processing test

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,30 @@
+import { TestInterface } from './interfaces.ts';
+import { TestCollectionInterface } from './interfaces.ts';
+
+
+function runTest(itemQuantity: number): TestCollectionInterface {
+  let res: TestCollectionInterface = [];
+  res.id = 1;
+  res.expirationDate = new Date();
+  for(let i = 0; i < itemQuantity; i++) {
+    const collectionItem: TestInterface = {
+      id: i,
+      label: `item${i}`
+    };
+    res.collection.push(collectionItem);
+  }
+  return res;
+}
+
+
+
+
+
 function init(): void {
   console.log('[INIT]');
-  //TODO logic here
+  
+  const collection: TestCollectionInterface = runTest(5);
+  console.log(collection);
 }
 
 init();
