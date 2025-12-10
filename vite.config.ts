1adfc8579ba5be469ca145dc90f2b889496fa17e
Abstractn <>
2025-12-10 18:52:43 +0100

    Disabled Redirect-Guard logic and edited Vite base path to not include dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  base: '/pipeline-test/dist/',
+  base: '/pipeline-test/',
   build: {
     //minify: 'terser',
     //minify: 'esbuild',
